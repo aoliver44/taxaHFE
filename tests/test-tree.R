8b7b6d515e2d3ed1ddcae32d603110286810ca06
Matt Kay <matthewckay@gmail.com>
2023-12-23 17:21:45 -0800

    rm test func, get ready

--- tests/test-tree.R
+++ tests/test-tree.R
@@ -1,11 +1,7 @@
 library(testthat)
-source("tree.R")
+# when ready to test tree.R, uncomment
+# source("tree.R")
 
-test_that("returns true if greater than 3", {
-    expect_true(greater_than_3(4))
-})
-
-
-test_that("returns false if less than 3", {
-    expect_false(greater_than_3(2))
+test_that("tests run", {
+    expect_true(TRUE)
 })
--- tree.R
+++ tree.R
@@ -887,10 +887,3 @@ load_docopt <- function(doc_string, version, to_convert) {
     simplify = FALSE
   ))
 }
-
-greater_than_3 <- function(n) {
-  if (n > 3) {
-    return(TRUE)
-  }
-  return(FALSE)
-}
