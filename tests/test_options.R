d6b1fd4cf3f0f90267c6ab6d0ed3f222ca286ef8
Matt Kay <matthewckay@gmail.com>
2024-11-02 18:53:48 -0400

    it fails when expected, fix test

--- tests/test_options.R
+++ tests/test_options.R
@@ -23,7 +23,7 @@ test_that("initialize_parser works", {
       .package = 'argparse'
     )
 
-    expect_error(parser$parse_args(flags), "wrong")
+    expect_error(parser$parse_args(flags), version)
   })
 
   test_that("parser prints help message when passed -h", {
