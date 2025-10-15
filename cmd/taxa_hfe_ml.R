4ce77e870a5a1557e595f6a60fef3fb8435d0bd4
Andrew Oliver <46385978+aoliver44@users.noreply.github.com>
2025-10-15 11:24:51 -0700

    Add in args for method_taxa_hfe_ml
    added args needed to specify writing_flattened_tree file (the "reasons" file), just for the training data.

--- cmd/taxa_hfe_ml.R
+++ cmd/taxa_hfe_ml.R
@@ -15,7 +15,7 @@ source("lib/methods.R")
 ## add commandline options =====================================================
 
 # to use this code line-by-line in the Rstudio context, commandArgs can be overloaded to specify the desired flags
-# ex. commandArgs <- function(x) { c("example_inputs/metadata.txt", "example_inputs/microbiome_data.txt", "-o", "example_outputs", "-s", "Sample", "-l", "Category", "-L", "3", "-n", "4", "--seed", "42", "--train_split", "0.8", "--tune_time", "0") }
+# ex. commandArgs <- function(x) { c("example_inputs/metadata.txt", "example_inputs/microbiome_data.txt", "-o", "example_outputs", "-s", "Sample", "-l", "Category", "-L", "3", "-n", "4", "--seed", "42", "--train_split", "0.8", "--tune_time", "0", "-W") }
 # these will be used by the argparser
 opts <- load_taxa_hfe_ml_args()
 
@@ -58,7 +58,9 @@ diet_ml_inputs <- method_taxa_hfe_ml(
   train_metadata = train_metadata,
   test_metadata = test_metadata,
   seed = opts$seed,
-  random_effects = opts$random_effects
+  random_effects = opts$random_effects,
+  write_flattened_tree = opts$write_flattened_tree,
+  output = opts$output_dir
 )
 
 # also run summarized levels if requested
