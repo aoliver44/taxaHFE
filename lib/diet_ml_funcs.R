4edb9f8360ea7195498559fe9da3b95108df782a
Andrew Oliver <46385978+aoliver44@users.noreply.github.com>
2026-02-04 09:36:15 -0800

    Log mean validation scores as well
    - do not log iteration 0, which is just a shotgun of parameters

--- lib/diet_ml_funcs.R
+++ lib/diet_ml_funcs.R
@@ -586,15 +586,15 @@ dietml_hp_tune <- function(diet_ml_workflow, model, parallel_workers, folds, typ
   
   ## log hyperparameters selected for less-complex, best model
   logger::log_info("Training parameters selected based on simplist model which optimize performance 
-                   (within 1 SE of best model). This helps prevent overfitting. Thus, you may see training performance 
-                   is VERY slightly lower than testing performance in some cases. 
+                   (within 1 SE of best model). This attempts to reduce overfitting. Thus, you may see training 
+                   performance is VERY slightly lower than testing performance in some cases. 
                    
                    In order to select a less complex model, for tree based models we select the models
                    with the highest min_node_size, ntree, and mtry parameters that produce a model within 1 SE
                    of the best tuned model.
                    
-                   For penalized regression, we select the model with the
-                   highest penalty that is 1 SE of a model with no constraints. 
+                   For penalized regression, we select the model with thehighest penalty that is 1 SE of a model 
+                   with no constraints. 
                    
                    These efforts are all aimed to decrease the overfitting of a trained model, potentially 
                    decreasing the generalizability gap (that is, the difference between the training and test score).")
@@ -602,6 +602,16 @@ dietml_hp_tune <- function(diet_ml_workflow, model, parallel_workers, folds, typ
   logger::log_info("Hyperparameters selected: ")
   for (n in seq(1:ncol(best_mod))) {
     logger::log_info(paste0(colnames(best_mod[n]), ": ", best_mod[1,n]))
+  }
+  
+  ## log mean validation scores
+  val_scores <- workflowsets::collect_metrics(search_res) %>% 
+    dplyr::filter(., .iter > 0) %>%
+    dplyr::group_by(., .metric) %>% 
+    dplyr::summarise(., mean_validation = mean(mean), mean_std_err = mean(std_err))
+  logger::log_info("Mean validation scores: ")
+  for (n in seq(1:nrow(val_scores))) {
+    logger::log_info(paste0(val_scores[n,1], " (validation): ", round(val_scores[n,2], 4), " (", round(val_scores[n,3], 4), " std err)"))
   }
   
   ## create the last model based on best parameters: Random Forest
